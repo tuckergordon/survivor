75dee93b212cd13d0a585226e1e73747a674d75e
Tucker Gordon <tgordon@Tuckers-MacBook-Pro.local>
2020-04-28 17:26:36 -0400

    round twenty results

--- src/app/results/round-twenty/round-twenty.component.ts
+++ src/app/results/round-twenty/round-twenty.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player, Round, Tribe } from 'src/app/shared/models/survivor.model';
 import { DataService, AggregateBy, SortBy } from 'src/app/shared/services/data.service';
+import { MatSort } from '@angular/material/sort';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-round-twenty',
@@ -10,20 +12,40 @@ import { DataService, AggregateBy, SortBy } from 'src/app/shared/services/data.s
 })
 export class RoundTwentyComponent implements OnInit {
   players$: Observable<Player[]>;
-  tribeScores$: Observable<Round[]>;
   eliminated$: Observable<Player[]>;
+
+  displayedColumns: string[] = ['name', 'score', 'tiebreak'];
 
   readonly ROUND = 20;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
-    this.tribeScores$ = this.dataService.getTribeScores(this.ROUND, AggregateBy.AVG, SortBy.DESC);
-    this.players$ = this.dataService.getPlayers({ round: this.ROUND });
+    this.players$ = this.dataService.getPlayers({ round: this.ROUND })
+      .pipe(
+        map(players => players.sort((a, b) => {
+          const aScore = this.getPlayerScore(a);
+          const bScore = this.getPlayerScore(b);
+          if (aScore === bScore) {
+            return this.getPlayerTiebreak(b) - this.getPlayerTiebreak(a);
+          }
+          if (isNaN(aScore)) return -1;
+          if (isNaN(bScore)) return 1;
+          return bScore - aScore;
+        }))
+      );
     this.eliminated$ = this.dataService.getRoundEliminated(this.ROUND);  // TODO: implement eliminated in refactor
   }
 
   getPlayerTribe(player: Player): Tribe {
     return DataService.getPlayerTribe(player, this.ROUND);
+  }
+
+  getPlayerScore(player: Player): number {
+    return DataService.getPlayerScore(player, this.ROUND);
+  }
+
+  getPlayerTiebreak(player: Player) {
+    return player['round' + this.ROUND + 'Tiebreak'];
   }
 }
